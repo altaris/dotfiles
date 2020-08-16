df8d4d440e69c0cf2d54b6644a101e1a116d87b5
Cédric HT <nope-mcnopeface@asdf.org>
2020-08-16 14:39:54 +0200

    Updated vscode extensions

--- vscode-extension-install.sh
+++ vscode-extension-install.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
-code --install-extension cschlosser.doxdocgen
-code --install-extension formulahendry.code-runner
+code --install-extension equinusocio.vsc-material-theme-icons
 code --install-extension James-Yu.latex-workshop
-code --install-extension lextudio.restructuredtext
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptools
-code --install-extension PKief.material-icon-theme
 code --install-extension pomber.git-file-history
-code --install-extension redhat.java
 code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension skyapps.fish-vscode
 code --install-extension stkb.rewrap
@@ -16,3 +11,4 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension streetsidesoftware.code-spell-checker-french
 code --install-extension timonwong.shellcheck
 code --install-extension tomoki1207.vscode-input-sequence
+code --install-extension VisualStudioExptTeam.vscodeintellicode
