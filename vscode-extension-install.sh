fd04ef413d8b36e25a32663030ba391bee6db914
Cédric HT <nope-mcnopeface@asdf.org>
2020-11-27 13:27:47 +0900

    Dotfile backup

--- vscode-extension-install.sh
+++ vscode-extension-install.sh
@@ -1,14 +1,1 @@
 #!/usr/bin/env sh
-code --install-extension equinusocio.vsc-material-theme-icons
-code --install-extension James-Yu.latex-workshop
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-python.python
-code --install-extension pomber.git-file-history
-code --install-extension shd101wyy.markdown-preview-enhanced
-code --install-extension skyapps.fish-vscode
-code --install-extension stkb.rewrap
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension streetsidesoftware.code-spell-checker-french
-code --install-extension timonwong.shellcheck
-code --install-extension tomoki1207.vscode-input-sequence
-code --install-extension VisualStudioExptTeam.vscodeintellicode
